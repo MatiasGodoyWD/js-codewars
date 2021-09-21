0356ea8e9ea6f5ecca5a1093562c674d240da206
Matias Godoy <matiasjuliangodoy@gmail.com>
2021-09-21 13:10:20 -0300

    Ejercicios al 21-9

--- sum-arr-with-str.js
+++ sum-arr-with-str.js
@@ -5,4 +5,37 @@
 const sumMix = (x) =>
   x.reduce((a, b) => (typeof b === "string" ? a + parseInt(b) : a + b), 0);
 
-console.log(sumMix([9, 3, "7", "3"]));
+function NumerosPositivos(arr) {
+  let positivos = [];
+  for (let i = 0; i <= arr.length; i++) {
+    if (arr[i] >= 0) {
+      positivos.push(arr[i]);
+    }
+  }
+  return positivos;
+}
+
+console.log(NumerosPositivos([1, -1, -3, 2, 3]));
+
+function NumerosPositivosForEach(arr) {
+  let positivos = [];
+  arr.forEach((n) => {
+    if (n >= 0) {
+      positivos.push(n);
+    }
+  });
+  return positivos;
+}
+
+console.log(NumerosPositivosForEach([1, -1, -3, 2, 3]));
+
+function NumerosPositivosFilter(arr) {
+  return arr.filter((n) => n >= 0);
+}
+
+console.log(NumerosPositivosFilter([1, -1, -3, 2, 3]));
+
+function NumerosPositivosReduce(arr) {
+  return arr.reduce((newArr, b) => (b > 0 ? [...newArr, b] : newArr), []);
+}
+console.log(NumerosPositivosReduce([1, -1, -3, 2, 3]));
